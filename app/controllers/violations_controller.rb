80b8710d7c3ab1f3660f1b2277de4ae183a2aadc
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-05-06 14:39:04 -0400

    added bronx

--- app/controllers/violations_controller.rb
+++ app/controllers/violations_controller.rb
@@ -8,14 +8,23 @@ class ViolationsController < ApplicationController
   end
 
   def manhattan
+    gon.data = Violation.get_bar_chart_data(1)
+    
+    gon.sunburst_data = Violation.get_sunburst_data(1)
 
   end
 
   def bronx
+    gon.data = Violation.get_bar_chart_data(2)
+    
+    gon.sunburst_data = Violation.get_sunburst_data(2)
 
   end
 
   def brooklyn
+    gon.data = Violation.get_bar_chart_data(3)
+    
+    gon.sunburst_data = Violation.get_sunburst_data(3)
 
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,18 @@
 
 client = SODA::Client.new({:domain => "data.cityofnewyork.us", :app_token => ENV["OPEN_DATA_APP_TOKEN"]})
 
-#manhattan borough data
+#manhattan borough data, id = 1
+#bk borough data, id = 3
+#bronx borough data, id = 2
 
-response = client.get("wvxf-dwi5", {:boroid => 1})
+
+response = client.get("wvxf-dwi5", {:boroid => 2})
 
 response.each do |r|
   violation = Violation.create(violation_id: r.violationid, building_id: r.buildingid, house_number: r.housenumber,
     street_name: r.streetname, apt_number: r.apartment, zip_code: r["zip"], approval_date: r.approveddate,
     current_status_date: r.currentstatusdate, current_status: r.currentstatus, boro: r.boro, 
-    description: r.novdescription[20..-1].downcase, boro_id: 1)
+    description: r.novdescription[20..-1].downcase, boro_id: 2)
 
   if r.novdescription.downcase.include?("mold")
     violation.keyword = "Mold"
